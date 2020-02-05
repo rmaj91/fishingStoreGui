8ccb2b23fd511db5837235fa5ab9fb3464094713
rmaj91 <rmaj91@gmail.com>
2020-02-05 17:21:01 +0100

    Add Register component draw

---
+++ src/components/Register.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+class Register extends Component{
+  render(){
+    return(
+      <div>
+        <input type="text"></input>
+        <input type="password"></input>
+      </div>
+    )
+  }
+}
+
+
+export default Register;
