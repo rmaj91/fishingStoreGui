5d71751693860ca22035702331ec4b68a55fa41b
rmaj91 <rmaj91@gmail.com>
2020-02-13 23:16:58 +0100

    Add post request for */auth/login

--- src/components/LoginBox.js
+++ src/components/LoginBox.js
@@ -1,13 +1,43 @@
 import React,{Component} from 'react';
 
 class Loginbox extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      email:'',
+      password:''
+    }
+  }
+
+  onChangeEmail = (e)=> this.setState({email: e.target.value})
+  onChangePassword = (e)=> this.setState({password: e.target.value})
+
+  onSubmit = (e)=>{
+    fetch('http://localhost:8080/v1/api/auth/login',{
+      method:'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify({
+        email:this.state.email,
+        password:this.state.password
+      })
+    })
+    .then((response) => response.json())
+    .then((data)=>{console.log('token: ', data.token)})
+    .catch((error)=> console.log('error while logging...'))
+
+    e.preventDefault();
+  }
+
   render(){
     return(
       <div id="loginBox">
-      <form>
-        <input type="text" placeholder="Email..." className="form-control"/><br/>
-        <input type="password" placeholder="Password..." className="form-control"/><br/>
-        <input type="submit" value="Login"></input>
+      <form onSubmit={this.onSubmit}>
+        <input type="text" onChange={this.onChangeEmail} value={this.state.email} placeholder="Email..." className="form-control"/><br/>
+        <input type="password" onChange={this.onChangePassword} value={this.state.password}placeholder="Password..." className="form-control"/><br/>
+        <input  type="submit" value="Login"></input>
       </form>
       </div>
     )
