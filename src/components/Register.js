045ecec35c40e39ad1c6f961df1c790cb6941767
rmaj91 <rmaj91@gmail.com>
2020-02-13 19:50:20 +0100

    Change login to email

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,20 +3,22 @@ import React,{Component} from 'react';
 class Register extends Component{
   render(){
     return(
-      <div style={{marginRight:'40%',marginTop:'20px'}}>
-        <label htmlFor="login">Email:</label><br/>
-        <input name="login" type="text"></input><br/>
+      <div style={{marginRight:'40%',marginTop:'20px'}} method="post">
+        <form>
+          <label htmlFor="email">Email:</label><br/>
+          <input name="email" type="text"></input><br/>
 
-        <label htmlFor="login">Password:</label><br/>
-        <input name="password" type="password"></input><br/>
+          <label htmlFor="password">Password:</label><br/>
+          <input name="password" type="password"></input><br/>
 
-        <label htmlFor="firstName">First Name:</label><br/>
-        <input name="firstName" type="text"></input><br/>
+          <label htmlFor="firstName">First Name:</label><br/>
+          <input name="firstName" type="text"></input><br/>
 
-        <label htmlFor="lastName">Last Name:</label><br/>
-        <input name="lastName" type="text"></input><br/>
+          <label htmlFor="lastName">Last Name:</label><br/>
+          <input name="lastName" type="text"></input><br/>
 
-        <input type="submit" value="Register"/>
+          <input type="submit" value="Register" />
+        </form>
       </div>
     )
   }
